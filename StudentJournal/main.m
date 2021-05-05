12cef7ee4370525aa769dee1eb0408efe29a567d
Maksim Karaseu <karaseu.maksim@gmail.com>
2021-05-05 10:10:05 +0300

    Initial Commit

---
+++ StudentJournal/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  StudentJournal
+//
+//  Created by … on 5.05.21.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
